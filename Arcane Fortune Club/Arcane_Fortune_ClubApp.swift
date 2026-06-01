e3aa1c89a995a433237e25811aeaa27af5824373
keshew <artyom.korotckov2017@yandex.ru>
2026-06-01 22:11:16 +0500

    Initial Commit

---
+++ Arcane Fortune Club/Arcane_Fortune_ClubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Arcane_Fortune_ClubApp.swift
+//  Arcane Fortune Club
+//
+//  Created by Артём Коротков on 01.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct Arcane_Fortune_ClubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
